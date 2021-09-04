6047249f0f5e928932640613fa0f08dcded53bef
Tejas <tejas14all@gmail.com>
2021-09-04 09:32:05 +0530

    delete user by id

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,33 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
- 
+	@DeleteMapping("/users/{userId}")
+	public ResponseEntity<UserEntity> deleteUserById(@PathVariable("userId") int userId) {
+
+		ResponseEntity<UserEntity> res = new ResponseEntity<>();
+		boolean isError = false;
+		try {
+			UserEntity user = userRepository.getById(userId);
+			if (user != null) {
+				userRepository.delete(user);
+				res.setData(user);
+				res.setMsg("user removed");
+				res.setStatus(200);
+			} else {
+				isError = true;
+			}
+
+		} catch (Exception e) {
+
+			isError = true;
+		}
+
+		if (isError) {
+			res.setData(null);
+			res.setMsg("Invalid UserId");
+			res.setStatus(-1);
+		}
+		return res;
+	}
+
 }
