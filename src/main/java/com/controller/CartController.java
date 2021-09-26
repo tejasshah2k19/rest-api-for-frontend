210530f1faaaa710685cc247e7b3d264d9e08ff8
Tejas <tejas14all@gmail.com>
2021-09-26 10:26:33 +0530

    remove item from cart

--- src/main/java/com/controller/CartController.java
+++ src/main/java/com/controller/CartController.java
@@ -3,6 +3,7 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,20 @@ public class CartController {
 		resp.setMsg("cart retervied");
 		resp.setStatus(200);
 		resp.setData(carts);
+		return resp;
+	}
+
+	@DeleteMapping("/carts/{cartId}")
+	public ResponseEntity<String> deleteCart(@PathVariable("cartId") int cartId) {
+
+		
+		cartRepo.deleteById(cartId);
+
+
+		ResponseEntity<String> resp = new ResponseEntity<>();
+		resp.setMsg("item removed from cart");
+		resp.setStatus(200);
+		resp.setData("");
 		return resp;
 	}
 
